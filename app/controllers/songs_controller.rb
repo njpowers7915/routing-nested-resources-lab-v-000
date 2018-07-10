93f6de361ca9b6eb367c15b1d2ccf73724f1ca65
Nicholas Powers <njpowers7915@gmail.com>
2018-07-10 01:18:25 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
     if Song.find_by_id(params[:id])
         @song = Song.find(params[:id])
     else
-        flash[:alert] = "Song does not exist"
+        flash[:alert] = "Song not found"
         redirect_to artist_songs_path
     end
   end
